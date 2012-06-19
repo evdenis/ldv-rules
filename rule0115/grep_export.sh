adad9f5efebb7737c76a7473ad91a485a910516f
Denis <work@linux-ri05.(none)>
2012-06-19 11:03:57 +0400

    fix

--- rule0115/grep_export.sh
+++ rule0115/grep_export.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-for i in $(find . -type f -name '*.[ch]')
+# $1 - linux kernel sources directory
+# $2 - export definitions file
+# $3 - export names file 
+
+rm -f "$2" "$3"
+
+for i in $(find "$1" -type f -name '*.[ch]')
 do
-	for j in $(sed -ne 's/^[[:space:]]*EXPORT_SYMBOL_GPL([[:blank:]]*\([[:alnum:]_]*\)[[:blank:]]*);.*$/\1/p' $i)
+	for j in $(sed -ne 's/^[[:space:]]*EXPORT_SYMBOL_GPL([[:blank:]]*\([[:alnum:]_]*\)[[:blank:]]*);.*$/\1/p' "$i")
 	do
-		
+		tmp=$(grep -oe "^\([[:alnum:]_]*\**[[:space:]]*\)*$j[[:space:]]*(" "$i" | sed -e '/^[[:space:]]*\**[[:space:]]*.*$/d' -e "/^[[:space:]]*$j.*$/d" )
+		if [[ -n "$tmp" ]]
+		then
+			echo "$tmp" | sed -e 's/(/(..)/' >> "$2"
+			echo "$j" >> "$3"
+		else
+			echo $j
+		fi
 	done
 done 
+
