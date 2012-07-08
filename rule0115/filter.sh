a071109182f45fff3966b216f583c026d101340e
Denis <work@linux-ri05.(none)>
2012-07-08 23:18:16 +0400

    Macroses with args filtering

--- rule0115/filter.sh
+++ rule0115/filter.sh
@@ -11,25 +11,33 @@ out="${3:-${file}}"
 tmp="$(mktemp)"
 #file_define="$(mktemp)"
 file_define="./rule_cache/macros_without_args"
-filter_define="./rule_cache/macros_filter"
+filter_define_wa="./rule_cache/macros_wa_filter"
+filter_define="./rule_cache/macros_filter_wa"
 
 #configuration
 filter_step=1000
 
 [[ ! -r "$file_define" ]] && ./extract_macros_filter.sh "$dir" "$file_define"
 
+#TODO: separate presets && blacklists
+if [[ ! -r "$filter_define_wa" ]]
+then
+   cat ./filter.preset > "$filter_define_wa"
+   #FIXME: explicit naming
+   grep -h -e '^__[a-z][a-z_]*$' "$file_define" >> "$filter_define_wa"
+   sort -bi -u -o "$filter_define_wa" "$filter_define_wa"
+   comm -23 "$filter_define_wa" <(cat ./filter.blacklist | sort -u) > "$tmp" && cp -f "$tmp" "$filter_define_wa"
+fi
 
+#TODO: separate presets && blacklists
 if [[ ! -r "$filter_define" ]]
 then
    cat ./filter.preset > "$filter_define"
    #FIXME: explicit naming
-   #TODO: proper filtering of function-like macros
-   #grep -h -e '^__[a-z][a-z_]*$' "$file_define" ./rule_cache/mnames >> "$filter_define"
-   grep -h -e '^__[a-z][a-z_]*$' "$file_define" >> "$filter_define"
+   grep -h -e '^__[a-z][a-z_]*$' "./rule_cache/mnames.raw" >> "$filter_define"
    sort -bi -u -o "$filter_define" "$filter_define"
    comm -23 "$filter_define" <(cat ./filter.blacklist | sort -u) > "$tmp" && cp -f "$tmp" "$filter_define"
 fi
-
 
 #attribute filter
 perl -n -e 's/__attribute__[ \t]*\((?<b>\((?:[^\(\)]|(?&b))*\))\)[ \t]*//g; print' "$file" > "$tmp"
@@ -37,13 +45,24 @@ sed -i -e 's/asmlinkage//g' "$tmp"
 
 #macros filter construction
 filter=''
+nol=$(cat "$filter_define_wa" | wc -l)
+for (( i=1; i<$nol; i+=$filter_step ))
+do
+   filter=$(tail -n +${i} "$filter_define_wa" | head -n $filter_step | tr '\n' '|' | sed -e 's/|$//')
+   if [[ -n "$filter" ]]
+   then
+      perl -i -n -e "s/\s*(?:$filter)(\s+)(?!\()/\1/g; print;" "$tmp"
+   fi
+done
+
+filter=''
 nol=$(cat "$filter_define" | wc -l)
 for (( i=1; i<$nol; i+=$filter_step ))
 do
    filter=$(tail -n +${i} "$filter_define" | head -n $filter_step | tr '\n' '|' | sed -e 's/|$//')
    if [[ -n "$filter" ]]
    then
-      perl -i -n -e "s/\s*(?:$filter)((\s|\*)+)(?!\()/\$1/g; print;" "$tmp"
+      perl -i -n -e "s/\s*(?:$filter)\s*(?<args>\((?:[^\(\)]|(?&args))+\))(?!\s*$)//g; print;" "$tmp"
    fi
 done
 
