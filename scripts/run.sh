0962a3e0b3db1c640988eee74fbd946ff1f91015
denis <efreniv@ispras.ru>
2012-07-11 16:28:03 +0400

    scripts rmsem && run

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+for i in block fs mm security virt crypto net sound
+do
+   LDV_DEBUG=100 ldv-manager "envs=linux-3.2.22.tar.bz2" kernel_driver=1 "drivers=$i/" "rule_models=113_1a" | tee log-$i.txt
+done
+
+for i in  $(find /home/work/workspace/linux-stable/drivers/ -maxdepth 1 -type d | cut -d '/' -f 6- | tail -n +2 | less)
+do
+   LDV_DEBUG=100 ldv-manager "envs=linux-3.2.22.tar.bz2" kernel_driver=1 "drivers=$i/" "rule_models=113_1a" | tee log-$i.txt
+done
+
+
