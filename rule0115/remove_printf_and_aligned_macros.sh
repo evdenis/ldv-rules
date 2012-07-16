ff684ef67917dc44880dbc1cf9e74895f8b17dd2
efremov <efremov@ispras.ru>
2012-07-16 12:53:20 +0400

    __acquire && __release problem

--- rule0115/remove_printf_and_aligned_macros.sh
+++ rule0115/remove_printf_and_aligned_macros.sh
@@ -6,5 +6,6 @@
 for i in $(find "$1" -type f -name '*.[ch]')
 do
    sed -i -e 's/__printf([[:space:]]*[[:digit:]]\+[[:space:]]*,[[:space:]]*[[:digit:]]\+[[:space:]]*)//g' -e 's/__aligned([[:space:]]*[[:digit:]]\+[[:space:]]*)//g' "$i"
+   perl -i -n -e '/(__acquire|__release)\(\s*(?!x)[\w->&]+\s*\)/; print;' "$i"
 done
 
