51037eac2b0acc379e51156b0bf8bee9e20c3f8f
efremov <efremov@ispras.ru>
2012-07-16 13:02:27 +0400

    upload_to_db update UNDONE

--- scripts/upload_to_db.sh
+++ scripts/upload_to_db.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-while file=$(inotifywait -e modify -e create --format '%w/%f' .)
-do
-    ldv-upload "$file" &
-done
+#declare -A desc
+
+#LDV_TASK_DESCRIPTION=
+#LDV_TASK_ID=
+
+upload ()
+{
+   local file="$1"
+   local upload=no
+   
+   if [[ -r "$file" ]]
+   then
+      if expr match "$file" '*.pax'
+      then
+         if grep -q -F -e 'UNSAFE' "$file"
+         then
+            upload=yes
+         elif grep -q -F -e 'SAFE' "$file"
+         then
+            upload=no
+         fi
+         ldv-upload "$file" > /dev/null
+      else
+         return 2
+      fi
+   else
+      echo "Error: $file is not readable."
+      return 1
+   fi
+   return 0
+}
+
+
+inotifywait --quiet --recursive --monitor -e modify -e create --format '%w/%f' . | xargs --max-lines=1 --max-procs=1 --no-run-if-empty upload
 
