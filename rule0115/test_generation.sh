d28748a265b2f180e34431edd65f96553e7d30af
Denis Efremov <yefremov.denis@gmail.com>
2012-08-02 12:41:45 +0400

    Test for massive rules generation added

---
+++ rule0115/test_generation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x 
+
+git_dir="$1"
+
+pushd "$git_dir" > /dev/null
+   for i in "$(git tag | grep -v 'rc\|tree' | sort -r )"
+   do
+      git checkout -f "$i"
+      popd > /dev/null
+         ./generate.sh 2 "$git_dir"
+      pushd "$git_dir" > /dev/null
+   done
+popd > /dev/null
+
