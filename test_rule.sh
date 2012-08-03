441864ea4b671afe71a055cc3d83af5bd1c330ef
Denis Efremov <efremov@ispras.ru>
2012-08-03 20:08:53 +0400

    test script update

--- test_rule.sh
+++ test_rule.sh
@@ -3,7 +3,7 @@
 tdir="$1"
 
 rule="0$2"
-rule_dir="model${rule}"
+rule_dir="rule${rule}"
 
 test_name="test${rule}.tar.bz2"
 
@@ -23,11 +23,11 @@ pushd "$rule_dir" > /dev/null
       fi
    fi
    
-   tar cjf "$test" ./test/
-   mv "$test" "$tdir"
+   tar cjf "$test_name" ./test/
+   mv "$test_name" "$tdir"
    
    pushd "$tdir" > /dev/null
-      LDV_DEBUG=100 LDV_VIEW=y ldv-manager "envs=linux-3.2.23.tar.bz2" "drivers=${test}" "rule_models=${rule}" | tee "log-${rule}.txt"
+      LDV_DEBUG=100 LDV_VIEW=y ldv-manager "envs=linux-3.2.23.tar.bz2" "drivers=${test_name}" "rule_models=${2}" | tee "log-${rule}.txt"
    popd > /dev/null
    
 popd > /dev/null
