05321de6a3f6954888bf666acc77b73744b4f837
Denis Efremov <efremov@ispras.ru>
2012-08-03 21:17:47 +0400

    run script update; multiple rules support

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ kver="$1"
 
 kfile="linux-${kver}.tar.bz2"
 
-rver="$2"
+rules="${@:2}"
 
 if [[ ! -r "$kfile" ]]
 then
@@ -13,13 +13,13 @@ fi
 
 for i in arch block fs mm security virt crypto net sound
 do
-   LDV_DEBUG=100 ldv-manager "envs=${kfile}" kernel_driver=1 "drivers=${i}/" "rule_models=${rver}" | tee "log-${i}.txt"
+   LDV_DEBUG=100 ldv-manager "envs=${kfile}" kernel_driver=1 "drivers=${i}/" "rule_models=${rules}" | tee "log-${i}.txt"
    rm -fr ./work/
 done
 
 for i in $(find ./inst/current/envs/linux-${kver}/linux-${kver}/drivers/ -maxdepth 1 -type d | cut -d '/' -f 6- | tail -n +2)
 do
-   LDV_DEBUG=100 ldv-manager "envs=${kfile}" kernel_driver=1 "drivers=${i}/" "rule_models=${rver}" | tee "log-${i}.txt"
+   LDV_DEBUG=100 ldv-manager "envs=${kfile}" kernel_driver=1 "drivers=${i}/" "rule_models=${rules}" | tee "log-${i}.txt"
    rm -fr ./work/
 done
 
