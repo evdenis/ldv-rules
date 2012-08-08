0a2352ad194fa61e20820dd6da65fa10fd75059f
Denis Efremov <efremov@ispras.ru>
2012-08-08 19:02:45 +0400

    BUG && BUG_ON exclusion from callgraph

--- generator/call.rb
+++ generator/call.rb
@@ -47,7 +47,9 @@ end
 graph = RGL::DirectedAdjacencyGraph.new
 root = Node.new(ARGV[1])
 graph.add_vertex(root)
-%x[pushd #{ARGV[0]} > /dev/null; cscope -d -L -3#{root.id} | cut -d ' ' -f 2 | sort -u; popd > /dev/null;].split( ' ' ).each { |v| graph.add_edge(root, Node.new(v)) }
+%x[pushd #{ARGV[0]} > /dev/null; cscope -d -L -3#{root.id} | cut -d ' ' -f 2 | sort -u | grep -v -e '^BUG\(_ON\)\?$'; popd > /dev/null;].split( ' ' ).each { |v|
+   graph.add_edge(root, Node.new(v))
+}
 root.mark
 
 vrarray = Array.new
@@ -71,7 +73,7 @@ loop do
    graph.each_vertex { |v|
       if not v.check
                 vrarray.push(v)
-             end
+      end
    }
    
    if vrarray.empty?
@@ -85,7 +87,7 @@ loop do
       lgraph = RGL::DirectedAdjacencyGraph.new
       varray = Array.new
       lgraph.add_vertex(lroot)
-      %x[pushd #{ARGV[0]} > /dev/null; cscope -d -L -3#{lroot.id} | cut -d ' ' -f 2 | sort -u; popd > /dev/null;].split( ' ' ).each { |v|
+      %x[pushd #{ARGV[0]} > /dev/null; cscope -d -L -3#{lroot.id} | cut -d ' ' -f 2 | sort -u | grep -v -e '^BUG\(_ON\)\?$'; popd > /dev/null;].split( ' ' ).each { |v|
          varray.push(Node.new(v))
       }
       semaphore.synchronize {
