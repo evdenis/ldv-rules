5ae96eba338d7a77e860598d270a11c2a2a0e052
Denis Efremov <efremov@ispras.ru>
2012-09-03 17:08:00 +0400

    fix for 0116 rule

--- rule0116/model0116.c
+++ rule0116/model0116.c
@@ -8,10 +8,13 @@ extern int LDV_IN_INTERRUPT;
 extern int ldv_lock_in_process_flag_TEMPLATE;
 extern int ldv_lock_in_interrupt_flag_TEMPLATE;
 
+/* LDV_COMMENT_MODEL_STATE*/
 int ldv_lock_in_process_flag_TEMPLATE = 0;
+/* LDV_COMMENT_MODEL_STATE*/
 int ldv_lock_in_interrupt_flag_TEMPLATE = 0;
 
-int ldv_irq_disable = 0;
+/* LDV_COMMENT_MODEL_STATE*/
+int ldv_irq_disable_nesting = 0;
 
 #define __ldv_lock_in_interrupt()               \
    do {                                         \
@@ -22,14 +25,15 @@ int ldv_irq_disable = 0;
 
 #define __ldv_lock_in_process()                                               \
    do {                                                                       \
-      if ( ( LDV_IN_INTERRUPT == 1 ) && ( ldv_irq_disable <= 0 ) ) {          \
+      if ( ( LDV_IN_INTERRUPT == 1 ) && ( ldv_irq_disable_nesting <= 0 ) ) {  \
          ++ldv_lock_in_process_flag_TEMPLATE;                                 \
       }                                                                       \
    } while(0)
 
-/* LDV_COMMENT_MODEL_FUNCTION_DEFINITION(name='ldv_check_final_state') Check the final state  */
+/* LDV_COMMENT_MODEL_FUNCTION_DEFINITION(name='ldv_check_final_state') */
 void ldv_check_final_state_TEMPLATE(void)
 {
+   /* LDV_COMMENT_ASSERT*/
    ldv_assert( ( ldv_lock_in_interrupt_flag_TEMPLATE == 0 ) ||  ( ldv_lock_in_process_flag_TEMPLATE == 0 ) );
 }
 
@@ -57,28 +61,27 @@ void ldv_spin_lock_irqsave_TEMPLATE(spinlock_t *lock, unsigned long flags)
 
 void ldv_local_irq_disable( void )
 {
-   ++ldv_irq_disable;
+   ++ldv_irq_disable_nesting;
 }
 
 void ldv_local_irq_enable( void )
 {
-   --ldv_irq_enable;
+   --ldv_irq_disable_nesting;
 }
 
 void ldv_local_irq_save( void )
 {
-   ++ldv_irq_disable;
+   ++ldv_irq_disable_nesting;
 }
 
 void ldv_local_irq_restore( void )
 {
-   --ldv_irq_enable;
+   --ldv_irq_disable_nesting;
 }
 
-/* LDV_COMMENT_MODEL_FUNCTION_DEFINITION(name='ldv_initialize') Initialize lock variables */
+/* LDV_COMMENT_MODEL_FUNCTION_DEFINITION(name='ldv_initialize') Initialize lock variables*/
 void ldv_initialize_TEMPLATE(void)
 {
-   /* LDV_COMMENT_ASSERT Initialize lock with initial model value*/
    ldv_lock_in_process_flag_TEMPLATE = 0;
    ldv_lock_in_interrupt_flag_TEMPLATE = 0;
 }
